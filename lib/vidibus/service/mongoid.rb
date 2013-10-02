4dc97e1a9135712848ebf6ec7572a58d2043380a
punkrats <andre@webwarelab.com>
2013-10-02 15:40:28 +0200

    Fix Boolean field type for Mongoid 4

--- lib/vidibus/service/mongoid.rb
+++ lib/vidibus/service/mongoid.rb
@@ -9,7 +9,7 @@ module Vidibus
         field :uuid
         field :function
         field :realm_uuid
-        field :this, :type => Boolean, :default => false
+        field :this, :type => ::Mongoid::Boolean, :default => false
 
         attr_accessor :nonce
         attr_encrypted :secret
